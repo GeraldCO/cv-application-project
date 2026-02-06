2242d7257f4a9e391441ee53259d5ba479cd62e3
Gerald <geraldcerd@gmail.com>
2026-02-05 22:46:44 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+import './App.css'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: (theme.vars ?? theme).palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Box sx={{ flexGrow: 1, width:1500 }} >
+      <Grid container spacing={2} >
+        <Grid size={8}>
+          <Item>size=8</Item>
+        </Grid>
+        <Grid size={4}>
+          <Item>size=4</Item>
+        </Grid>
+        <Grid size={4}>
+          <Item>size=4</Item>
+        </Grid>
+        <Grid size={8}>
+          <Item>size=8</Item>
+        </Grid>
+      </Grid>
+    </Box>
+    </>
+  )
+}
+
+export default App
